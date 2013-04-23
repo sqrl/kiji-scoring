056b50ac5ceed1058e8538543bc01640cc047b53
Aaron Feldstein <afeldstein@wibidata.com>
2013-04-23 14:50:18 -0700

    Adds TestFreshnessDemo.

---
+++ src/test/java/org/kiji/scoring/TestFreshnessDemo.java
@@ -0,0 +1,82 @@
+package org.kiji.scoring;
+
+import static org.junit.Assert.assertEquals;
+
+import java.io.IOException;
+
+import org.junit.Test;
+
+import org.kiji.mapreduce.produce.KijiProducer;
+import org.kiji.mapreduce.produce.ProducerContext;
+import org.kiji.schema.EntityId;
+import org.kiji.schema.Kiji;
+import org.kiji.schema.KijiClientTest;
+import org.kiji.schema.KijiDataRequest;
+import org.kiji.schema.KijiRowData;
+import org.kiji.schema.KijiTable;
+import org.kiji.schema.KijiTableReader;
+import org.kiji.schema.KijiTableWriter;
+import org.kiji.schema.layout.KijiTableLayouts;
+import org.kiji.scoring.impl.InternalFreshKijiTableReader;
+
+/**
+ * Demo of KijiScoring freshening.
+ */
+public class TestFreshnessDemo extends KijiClientTest {
+
+  private static final class DemoProducer extends KijiProducer {
+
+    public KijiDataRequest getDataRequest() {
+      return KijiDataRequest.create("info", "visits");
+    }
+
+    public String getOutputColumn() {
+      return "info:visits";
+    }
+
+    public void produce(final KijiRowData input, final ProducerContext context) throws IOException {
+      final Long oldValue = input.getMostRecentValue("info", "visits");
+      context.put(oldValue + 1L);
+    }
+  }
+
+  @Test
+  public void testDemo() throws IOException {
+    // Get a Kiji instance.
+    final Kiji kiji = getKiji();
+    // Create the "user" table.
+    kiji.createTable(KijiTableLayouts.getLayout(KijiTableLayouts.COUNTER_TEST));
+    // Get a table from the Kiji instance.
+    final KijiTable table = kiji.openTable("user");
+    // Get a KijiFreshnessManager for the Kiji instance.
+    final KijiFreshnessManager manager = KijiFreshnessManager.create(kiji);
+    // Create a ShelfLife freshness policy and load a 1 day shelf life duration.
+    final KijiFreshnessPolicy policy = new ShelfLife(86400000);
+    // Store the freshness policy in the meta table for the table "user" and column "info:visits"
+    // using the ShelfLife freshness policy created above and the DemoProducer.
+    manager.storePolicy("user", "info:visits", DemoProducer.class, policy);
+    // Open a FreshKijiTableReader for the table with a timeout of 100 milliseconds.
+    // Note: the FreshKijiTableReader must be opened after the freshness policy is registered.
+    final FreshKijiTableReader freshReader = new InternalFreshKijiTableReader(table, 100);
+
+    // Write an old value to the cell we plan to request with timestamp 1 and value 10.
+    final EntityId eid = table.getEntityId("foo");
+    final KijiTableWriter writer = table.openTableWriter();
+    writer.put(eid, "info", "visits", 1L, 10L);
+    writer.close();
+
+    // Create a data request for the desired column.
+    final KijiDataRequest request = KijiDataRequest.create("info", "visits");
+
+    // Read from the table and get back a freshened value because 1L is more than a day ago.
+    assertEquals(11L, freshReader.get(eid, request).getMostRecentValue("info", "visits"));
+    // Read again and get back the same value because the DemoProducer wrote a new value.
+    assertEquals(11L, freshReader.get(eid, request).getMostRecentValue("info", "visits"));
+
+    // Cleanup
+    freshReader.close();
+    manager.close();
+    table.release();
+    kiji.release();
+  }
+}
